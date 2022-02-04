be52a5c59c092396b9ff70de092dae9ff923e2ef
Sensei <frontendsensei612@gmail.com>
2022-02-04 14:18:02 +0200

    Add general styles

--- games/lockpicking/src/App.vue
+++ games/lockpicking/src/App.vue
@@ -1,12 +1,16 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
   <router-view />
 </template>
 
 <style>
+* {
+  box-sizing: border-box;
+}
+
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
