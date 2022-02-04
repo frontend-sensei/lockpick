35f67b6bcfb775f0825e25d0f2f389fd113fadd3
Yaroslav Gulnazaryan <43448610+frontend-sensei@users.noreply.github.com>
2022-02-04 15:30:19 +0200

    Game-page (#7)
    * Add game route
    
    * Add LockPicking component
    
    * Display LockPicking component at Game page

---
+++ games/lockpicking/src/components/LockPicking.vue
@@ -0,0 +1,3 @@
+<template>
+  <div>Lock picking component</div>
+</template>
---
+++ games/lockpicking/src/views/Game.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="game-page">
+    <LockPicking />
+  </div>
+</template>
+
+<script>
+import LockPicking from "@/components/LockPicking";
+
+export default {
+  components: { LockPicking },
+  setup() {},
+};
+</script>
+
+<style>
+.game-page {
+  min-height: 100vh;
+  background-color: #121d2b;
+}
+</style>
