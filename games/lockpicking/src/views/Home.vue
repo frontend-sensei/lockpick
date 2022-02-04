29d738d70781abf14e04c8c403acfa70cfbcc287
Sensei <frontendsensei612@gmail.com>
2022-02-04 14:19:07 +0200

    Add home content and video for background

--- games/lockpicking/src/views/Home.vue
+++ games/lockpicking/src/views/Home.vue
@@ -1,5 +1,22 @@
 <template>
-  <div class="home"></div>
+  <div class="home-page">
+    <video
+      class="video-background"
+      :src="require('@/assets/video/bg.mp4')"
+      loop
+      muted
+      autoplay
+    ></video>
+    <h1>Game name</h1>
+    <h2>Hello player!</h2>
+    <p>In this game you can try yourself as lockpicker.</p>
+    <h4>How to play:</h4>
+    <p>
+      Just press space in the right moment. You have limited time for lock pick.
+    </p>
+    <p>Are you ready?</p>
+    <button>play</button>
+  </div>
 </template>
 
 <script>
@@ -7,3 +24,20 @@ export default {
   name: "Home",
 };
 </script>
+
+<style>
+.home-page {
+  color: #fff;
+}
+.video-background {
+  z-index: -1;
+  position: fixed;
+  top: 0;
+  right: 0;
+  left: 0;
+  bottom: 0;
+  object-fit: cover;
+  width: 100%;
+  height: 100%;
+}
+</style>
