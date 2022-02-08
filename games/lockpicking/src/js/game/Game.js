51afdecd0f8f9d3df3536ca25f43ff2058d8c72d
Sensei <frontendsensei612@gmail.com>
2022-02-08 23:44:23 +0200

    Refactor Game class, delete GameService

--- games/lockpicking/src/js/game/Game.js
+++ games/lockpicking/src/js/game/Game.js
@@ -1,18 +1,84 @@
 import { GameUI } from "./GameUI.js";
-import { GameService } from "./GameService.js";
 import { GameTimer } from "./GameTimer.js";
-
-const vendors = {
-  _UIClass: GameUI,
-  _TimerClass: GameTimer,
-};
 
 /**
  * Creates a new Game
  * @class Game
  */
 export class Game {
-  constructor(options) {
-    return new GameService(options, vendors);
+  /**
+   * @param {Object} level
+   */
+  constructor(level) {
+    this.attemts = 3;
+    this.launched = false;
+    this.level = level;
+
+    this.keydownHandler = this.keydownHandler.bind(this);
+    this.pendingHandler = false;
+
+    this._ui = new GameUI();
+    this._timer = new GameTimer(this, 2000);
+    this.render();
+  }
+
+  render() {
+    this._ui.render(".game-page");
+  }
+
+  start() {
+    this.launched = true;
+    this.addListeners();
+
+    this._timer.start();
+    this._ui._Bar._BarUI.movePointer();
+  }
+
+  stop() {
+    this._timer.stop();
+    this.launched = false;
+    this.removeListeners();
+    this._ui._Bar._BarUI.stopPointer();
+  }
+
+  onDefeat() {
+    this.launched = false;
+    this.removeListeners();
+    this._ui._Bar._BarUI.stopPointer();
+    this.gameOver();
+  }
+
+  gameOver() {
+    // Show popup
+    // redirect to home page
+  }
+
+  addListeners() {
+    window.addEventListener("keydown", this.keydownHandler);
+  }
+
+  removeListeners() {
+    window.removeEventListener("keydown", this.keydownHandler);
+  }
+
+  keydownHandler() {
+    if (this.pendingHandler) {
+      return;
+    }
+
+    this.pendingHandler = true;
+
+    if (this._timer.finished) {
+      return;
+    }
+    if (event.keyCode === 32) {
+      this._timer.pause();
+      this._ui._Bar._BarUI.stopPointer();
+      setTimeout(() => {
+        this._timer.start();
+        this._ui._Bar._BarUI.movePointer();
+        this.pendingHandler = false;
+      }, 1500);
+    }
   }
 }
