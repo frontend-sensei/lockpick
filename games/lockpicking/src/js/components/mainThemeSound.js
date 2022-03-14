734ae5529b173e20648e568f774e6eb4e8ff7ac4
Yaroslav Gulnazaryan <43448610+frontend-sensei@users.noreply.github.com>
2022-03-15 00:28:00 +0200

    add saving sound preferences to the localStorage (#65)

--- games/lockpicking/src/js/components/mainThemeSound.js
+++ games/lockpicking/src/js/components/mainThemeSound.js
@@ -1,21 +1,27 @@
-const contoller = document.querySelector(".sound-controller");
 const audio = new Audio("../../assets/sounds/main_theme.mp3");
 const audioVolume = 0.1;
 audio.loop = true;
 audio.autoplay = true;
 audio.volume = audioVolume;
 
+const audioMuted = JSON.parse(localStorage.getItem("isMuted")) || false;
+const contoller = document.querySelector(".sound-controller");
 const constraints = {
   audio: true,
 };
 
 navigator.mediaDevices.getUserMedia(constraints).then(() => {
   audio.play();
-  contoller.classList.toggle("sound-controller--unmuted");
-  contoller.classList.toggle("sound-controller--muted");
+  if (!audioMuted) {
+    toggleContollerState();
+  }
+  if (audioMuted) {
+    audio.volume = 0;
+  }
+
   contoller.addEventListener("click", () => {
-    contoller.classList.toggle("sound-controller--unmuted");
-    contoller.classList.toggle("sound-controller--muted");
+    localStorage.setItem("isMuted", !!audio.volume);
+    toggleContollerState();
     if (audio.volume) {
       audio.volume = 0;
       return;
@@ -23,3 +29,8 @@ navigator.mediaDevices.getUserMedia(constraints).then(() => {
     audio.volume = audioVolume;
   });
 });
+
+function toggleContollerState() {
+  contoller.classList.toggle("sound-controller--unmuted");
+  contoller.classList.toggle("sound-controller--muted");
+}
