5d81286e8da2bd00c7b7947d37f2cd3f82b25486
Sensei <frontendsensei612@gmail.com>
2022-03-14 23:36:47 +0200

    make unlock pin button full screen

--- games/lockpicking/src/js/game/mobile-unlock-btn/MobileUnlockBtn.js
+++ games/lockpicking/src/js/game/mobile-unlock-btn/MobileUnlockBtn.js
@@ -7,9 +7,8 @@ export class MobileUnlockBtn {
   }
 
   render() {
-    const element = document.createElement("div");
-    element.innerHTML = `<button class="mobile-unlock-btn">Unlock pin</button>`;
-    element.className = "mobile-unlock-btn-wrapper";
+    const element = document.createElement("button");
+    element.className = "mobile-unlock-btn";
     element.id = uniqueId();
     this.root._ui.node.appendChild(element);
     this.node = document.getElementById(element.id);
