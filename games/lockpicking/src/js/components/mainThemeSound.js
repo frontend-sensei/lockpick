e8eb172b393f752bcd75e56a895d9210aba264ff
Sensei <frontendsensei612@gmail.com>
2022-03-14 19:06:57 +0200

    add mute / unmute sound contoller

--- games/lockpicking/src/js/components/mainThemeSound.js
+++ games/lockpicking/src/js/components/mainThemeSound.js
@@ -1,7 +1,9 @@
+const contoller = document.querySelector(".sound-controller");
 const audio = new Audio("../../assets/sounds/main_theme.mp3");
+const audioVolume = 0.1;
 audio.loop = true;
 audio.autoplay = true;
-audio.volume = 0.1;
+audio.volume = audioVolume;
 
 const constraints = {
   audio: true,
@@ -9,4 +11,15 @@ const constraints = {
 
 navigator.mediaDevices.getUserMedia(constraints).then(() => {
   audio.play();
+  contoller.classList.toggle("sound-controller--unmuted");
+  contoller.classList.toggle("sound-controller--muted");
+  contoller.addEventListener("click", () => {
+    contoller.classList.toggle("sound-controller--unmuted");
+    contoller.classList.toggle("sound-controller--muted");
+    if (audio.volume) {
+      audio.volume = 0;
+      return;
+    }
+    audio.volume = audioVolume;
+  });
 });
