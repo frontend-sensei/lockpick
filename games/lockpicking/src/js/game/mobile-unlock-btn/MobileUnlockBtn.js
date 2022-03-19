47c21108ab4159c749d189fc6833d2d4249b3c34
Sensei <frontendsensei612@gmail.com>
2022-03-19 21:12:09 +0200

    refactored MobileUnlockBtn component and add jsDoc

--- games/lockpicking/src/js/game/mobile-unlock-btn/MobileUnlockBtn.js
+++ games/lockpicking/src/js/game/mobile-unlock-btn/MobileUnlockBtn.js
@@ -1,11 +1,18 @@
 import { uniqueId } from "../../utils/uniqueId.js";
 
+/**
+ * Creates a Mobile Unlock button.
+ * @class MobileUnlockBtn
+ */
 export class MobileUnlockBtn {
   constructor(root) {
     this.root = root;
     this.node = null;
   }
 
+  /**
+   * @public
+   */
   render() {
     const element = document.createElement("button");
     element.className = "mobile-unlock-btn";
