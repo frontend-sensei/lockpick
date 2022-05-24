8643596d8f70a5854144235ff8e1d59f01db97fc
frontend-sensei <frontendsensei612@gmail.com>
2022-05-25 00:06:04 +0300

    Add start method to the Standard mode

--- games/lockpicking/src/js/game/modes/Standard.js
+++ games/lockpicking/src/js/game/modes/Standard.js
@@ -1,7 +1,17 @@
 import { Observable } from "../../utils/observable.js";
+import { Countdown } from "../countdown/Countdown.js";
 
 export class StandardMode {
   constructor(root) {
+    this.root = root;
     this.attempts = new Observable(3)
+  }
+
+  async start() {
+    await new Countdown(this.root).start();
+    this.root._listeners.register();
+    this.root._timer.start();
+    this.root._ui._Bar.movePointer();
+    this.root._ui._Lockpick.animate();
   }
 }
