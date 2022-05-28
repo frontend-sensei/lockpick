e89614e0b20407aadf41993ffdb2dbd57762497f
frontend-sensei <frontendsensei612@gmail.com>
2022-05-28 17:13:11 +0300

    Add register condition

--- games/lockpicking/src/js/game/listeners/DesktopListeners.js
+++ games/lockpicking/src/js/game/listeners/DesktopListeners.js
@@ -1,13 +1,19 @@
 export class DesktopListeners {
   constructor(listener) {
     this.listener = listener;
+    this.registered = false;
   }
 
   register() {
+    if(this.registered) {
+      return
+    }
+    this.registered = true
     window.addEventListener("keydown", this.listener);
   }
 
   remove() {
+    this.registered = false
     window.removeEventListener("keydown", this.listener);
   }
 }
